56e74b7d41853e6b5b7c3ee151b0fd71cecee0ea
Vikas Tiwari <vikas.tiwari990@gmail.com>
2020-04-12 15:01:45 +0530

    initializing kafka project

---
+++ src/main/java/com/kafka/home/ProducerDemo.java
@@ -0,0 +1,28 @@
+package com.kafka.home;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        //create producer properties
+        String bootstrapServer = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String>  record = new ProducerRecord<String, String>("first_topic", "Hello World");
+
+        //send Data -- asynchronous
+        producer.send(record);
+        producer.flush();
+        producer.close();
+    }
+}
